4a2dab8a1db1ea29738b45dcc8d6f23cd5da3dc1
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-05-05 13:30:09 -0400

    added request log

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const mongoose = require('mongoose')
 const excerciseRouter = require('./routes/excercise')
 const usersRouter = require('./routes/users')
 const resultRouter = require('./routes/result')
+const { logger } = require('./middleware/logEvents')
+const  errorHandler  = require('./middleware/errorHandler')
+
 
 require('dotenv').config()
 const app = express()   
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
-
+app.use(logger)
 const uri = process.env.ATLAS_URI
 mongoose.connect(uri)
 
@@ -23,6 +26,8 @@ connection.once('open', ()=> {
 app.use('/excercises', excerciseRouter)
 app.use('/users', usersRouter)
 app.use('/results', resultRouter)
+
+app.use(errorHandler);
 
 app.listen(port, ()=> {
     console.log('listening on port', port)
